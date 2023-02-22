caea68df468db8fdb244da1071a99934fce3aff5
rafec <rafael.brizuena@gmail.com>
2023-02-21 22:16:40 -0400

    first commit

---
+++ src/com/dio/FirstProgram.java
@@ -0,0 +1,24 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		/*
+		 * int a = 2; int b = 3; System.out.println("Hello World! " + (a+b));
+		 */
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String idade;
+}
